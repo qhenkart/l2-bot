d8a8c996e6cccbbc9fc646793ab65e9ec869d085
Quest Henkart <qhenkart@gmail.com>
2018-01-08 12:51:52 +0700

    remove unecessary files

--- errs/errs.go
+++ errs/errs.go
@@ -2,7 +2,6 @@ package errs
 
 import (
 	"fmt"
-	"net/http"
 )
 
 // ErrInfo matches the error interface but provides more information
@@ -13,11 +12,6 @@ type ErrInfo struct {
 	ErrCode    int
 	ExitStatus *int
 	Command    string
-	// StatusCode is used for setting corresponding http response
-	// for the error
-	StatusCode int `json:"-"`
-	// Context is used for giving a detailed information about the error
-	context error
 }
 
 // Error is used for implementing the error interface, and for creating
@@ -46,51 +40,3 @@ func NewCommandFailedErr(command string) *ErrInfo {
 		Command: command,
 	}
 }
-
-// ErrorWithContext is used for creating errors with their context. For instance, we can have
-// an ErrSendGrid error, and we can define specific error reason into ErrSendGrid as a context.
-// So it becomes: send grid error: could not connect to api
-func ErrorWithContext(parentErr, err error) *ErrInfo {
-	castErr, ok := parentErr.(*ErrInfo)
-	customErr := new(ErrInfo)
-
-	if ok {
-		*customErr = *castErr
-	} else {
-		customErr.Err = parentErr.Error()
-	}
-
-	customErr.context = err
-
-	return customErr
-}
-
-// ErrorWithStatus is used for creating errors with their corresponding http status codes.
-func ErrorWithStatus(message string, errorCode int, statusCode int) *ErrInfo {
-	return &ErrInfo{
-		Err:        message,
-		ErrCode:    errorCode,
-		StatusCode: statusCode,
-	}
-}
-
-// Context is used for creating a new instance of the error and appending a context into that
-func (e ErrInfo) Context(err error) *ErrInfo {
-	ctxErr := new(ErrInfo)
-	*ctxErr = e
-	ctxErr.context = err
-
-	return ctxErr
-}
-
-var errOperationFailed = &ErrInfo{Err: "operation failed"}
-var errCommandFailed = &ErrInfo{Err: "command failed"}
-
-// ErrMarshalData is sent when marshal could not work
-var ErrMarshalData = ErrorWithStatus("could not marshal data", 102, http.StatusBadRequest)
-
-// ErrServiceRequest fires when a request is unable to reach its target
-var ErrServiceRequest = ErrorWithStatus("service request error", 105, http.StatusInternalServerError)
-
-// ErrDecoder defines a common decoder error
-var ErrDecoder = ErrorWithStatus("could not decode data", 101, http.StatusBadRequest)
